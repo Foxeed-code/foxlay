e7cca6adf8f29cce569d5a0dd68dd4364e2c57f1
Foxeed <foxeedbackup@gmail.com>
2024-12-26 16:44:23 +0530

    Change options in home screen

--- lib/layout/home_screen.dart
+++ lib/layout/home_screen.dart
@@ -72,7 +72,7 @@ class _HomeScreenState extends State<HomeScreen> {
     'Pears',
     'Pineapple',
     'Raspberries',
-    'Strawberries',
+    'Strawberries pieces',
     'Watermelon pieces',
   ];
 
